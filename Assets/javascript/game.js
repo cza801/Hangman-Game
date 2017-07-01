0afd2798a8fee7a109dbe7f88aad6997d6311116
Cesar <cesssar626@gmail.com>
2017-06-30 23:44:56 -0600

    What i have to turn in

--- Assets/javascript/game.js
+++ Assets/javascript/game.js
@@ -3,6 +3,18 @@ var carMake = ['toyota','honda','ford','volkswagen', 'subaru'];
 var rand = carMake[Math.floor(Math.random() * carMake.length)];
 console.log(rand)
 
+
 document.onkeyup = function(start){
-	document.getElementById('unguessed').innerHTML = rand;
+
+	var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+        'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+        't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    var word;
+    var space;
+    var counter;
+    var lives;
+
+ 
+    
+	 document.getElementById('unguessed').innerHTML = rand;
 }
